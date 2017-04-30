8f50ade883c162866556c4cea89785d6a4b301ec
Alexander Gemsa <alexander.gemsa@gmail.com>
2017-04-30 21:38:12 +0200

    Update and rename lineage.mk to aokp.mk

--- aokp.mk
+++ aokp.mk
@@ -7,9 +7,9 @@ $(call inherit-product, device/samsung/klimtwifi/device.mk)
 PRODUCT_BRAND := samsung
 
 # Inherit some common lineage stuff
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/aokp/configs/common_full_tablet_wifionly.mk)
 
-PRODUCT_NAME := lineage_klimtwifi
+PRODUCT_NAME := aokp_klimtwifi
 PRODUCT_DEVICE := klimtwifi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
